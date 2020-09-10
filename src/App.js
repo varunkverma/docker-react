da1f009d9bbfa51ddebd496b77cacc2c171c40cc
Varun <varunkverma1996@gmail.com>
2020-09-11 01:14:20 +0530

    fixed issues with app text

--- src/App.js
+++ src/App.js
@@ -8,8 +8,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I deployed it
+            Deployed on AWS using travis cli
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
